f593ac7b9275716d9007746ba763d2cbf7031e7e
Jessie Liu <jessieqliu@google.com>
2024-12-18 19:40:33 +0000

    Add gcpcredential library

---
+++ server/gcpcredential/validate.go
@@ -0,0 +1,114 @@
+package gcpcredential
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	_ "embed"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"google.golang.org/api/idtoken"
+	"google.golang.org/api/option"
+)
+
+//go:embed data/roots.pem
+var googleCAPEM []byte
+
+// The certificates are downloaded from https://pki.goog/faq/#faq-27.
+// Note the guidance is to update at least semi-annually.
+func GoogleCACerts() (*x509.CertPool, error) {
+	certs := x509.NewCertPool()
+	if !certs.AppendCertsFromPEM(googleCAPEM) {
+		return nil, errors.New("failed to parse Google CA certificates")
+	}
+
+	return certs, nil
+}
+
+// ValidateAndParse validates each of the provided credentials, then returns the emails of the successfully verified tokens.
+// If an http.Client is provided, it will be used to initialize the idtoken validation client.
+func ValidateAndParse(ctx context.Context, client *http.Client, credentials []string, expectedAudience string) ([]string, error) {
+	if client == nil {
+		ca, err := GoogleCACerts()
+		if err != nil {
+			return nil, err
+		}
+		client = &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					RootCAs:    ca,
+					MinVersion: tls.VersionTLS13,
+				},
+			},
+		}
+	}
+
+	validatorOptions := []idtoken.ClientOption{
+		option.WithoutAuthentication(),
+		option.WithHTTPClient(client),
+	}
+
+	v, err := idtoken.NewValidator(ctx, validatorOptions...)
+	if err != nil {
+		return nil, fmt.Errorf("could not create ID token validator: %v", err.Error())
+	}
+
+	var emailClaims []string
+
+	for i, token := range credentials {
+		payload, err := v.Validate(ctx, token, expectedAudience)
+		if err != nil {
+			return nil, fmt.Errorf("invalid ID token in position %v: %v, token %s", i, err.Error(), token)
+		}
+
+		tokenClaims, err := parseClaims(payload)
+		if err != nil {
+			fmt.Printf("Error with ID token in position %v: %v", i, err)
+			continue
+		}
+
+		if tokenClaims.Email == "" {
+			fmt.Printf("ID token in position %v has no email claim\n", i)
+			continue
+		}
+
+		if !tokenClaims.EmailVerified {
+			fmt.Printf("email claim for ID token in position %v is not verified\n", i)
+			continue
+		}
+
+		emailClaims = append(emailClaims, tokenClaims.Email)
+	}
+
+	return emailClaims, nil
+}
+
+// parse takes an idtoken.Payload, which stores claims in a map[string]any. We want to
+// interpret the claims as a googleClaims struct. Instead of manually inspecting the map we just
+// encode/decode via JSON.
+// This is valid because the original claims were decoded from JSON (as part of the JWT).
+// claims.go
+func parseClaims(payload *idtoken.Payload) (*claims, error) {
+	data, err := json.Marshal(payload.Claims)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal JSON: %w", err)
+	}
+	claims := &claims{}
+	if err = json.Unmarshal(data, claims); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal claims: %w", err)
+	}
+	return claims, nil
+}
+
+// The subset of claims we care about in Google-issued OpenID tokens.
+// Full claims documented at:
+//
+//		https://cloud.google.com/compute/docs/instances/verifying-instance-identity#payload
+//		https://developers.google.com/identity/protocols/oauth2/openid-connect
+type claims struct {
+	Email         string `json:"email"`
+	EmailVerified bool   `json:"email_verified"`
+}
--- server/go.mod
+++ server/go.mod
@@ -6,20 +6,36 @@ require (
 	github.com/google/go-cmp v0.6.0
 	github.com/google/go-configfs-tsm v0.3.3-0.20240910040719-1cc7e25d9272
 	github.com/google/go-eventlog v0.0.2-0.20241213203620-f921bdc3aeb0
-	github.com/google/go-tpm v0.9.0
+	github.com/google/go-tdx-guest v0.3.2-0.20241217050406-c121d9d550ac
 	github.com/google/go-tpm-tools v0.4.4
-	google.golang.org/protobuf v1.34.2
+	google.golang.org/api v0.213.0
+	google.golang.org/protobuf v1.35.2
 )
 
 require (
-	github.com/golang/protobuf v1.5.3 // indirect
+	cloud.google.com/go/auth v0.13.0 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.6 // indirect
+	cloud.google.com/go/compute/metadata v0.6.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-sev-guest v0.9.3 // indirect
-	github.com/google/go-tdx-guest v0.3.2-0.20241217050406-c121d9d550ac // indirect
+	github.com/google/go-tpm v0.9.0 // indirect
 	github.com/google/logger v1.1.1 // indirect
-	github.com/google/uuid v1.3.1 // indirect
-	github.com/pborman/uuid v1.2.1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/google/s2a-go v0.1.8 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
+	github.com/googleapis/gax-go/v2 v2.14.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
+	go.opentelemetry.io/otel v1.29.0 // indirect
+	go.opentelemetry.io/otel/metric v1.29.0 // indirect
+	go.opentelemetry.io/otel/trace v1.29.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/net v0.32.0 // indirect
+	golang.org/x/oauth2 v0.24.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241209162323-e6fa225c2576 // indirect
+	google.golang.org/grpc v1.67.1 // indirect
 )
