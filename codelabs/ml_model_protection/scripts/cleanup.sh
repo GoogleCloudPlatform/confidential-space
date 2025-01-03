c8112c505de47ecbb03933a3fb15e3b70ace05a9
Keith Moyer <kmoy@google.com>
2025-01-03 10:05:23 -0800

    Remove stray quotations in cleanup script
    There were two extra quotations (line 59 and 107) that were breaking this script.
    
    Also remove unnecessary quotes inside of quotes that only complicate the script readability and parsing without any benefit.
    
    PiperOrigin-RevId: 711783983

--- codelabs/ml_model_protection/scripts/cleanup.sh
+++ codelabs/ml_model_protection/scripts/cleanup.sh
@@ -23,40 +23,40 @@ source common.sh
 #######################################
 delete_primus_company_resources() {
   if [ ! -z "${PRIMUS_PROJECT_ID}" ]; then
-    echo "PRIMUS_PROJECT_ID is set to "${PRIMUS_PROJECT_ID}""
+    echo "PRIMUS_PROJECT_ID is set to ${PRIMUS_PROJECT_ID}"
   else
     err "PRIMUS_PROJECT_ID is not set, please set the PRIMUS_PROJECT_ID with GCP project-id of Primus company."
   fi
   set_gcp_project "${PRIMUS_PROJECT_ID}"
 
-  confirmation=$(get_confirmation "Are you sure you want to delete the bucket "${PRIMUS_INPUT_STORAGE_BUCKET}" ?")
+  confirmation=$(get_confirmation "Are you sure you want to delete the bucket ${PRIMUS_INPUT_STORAGE_BUCKET} ?")
   if [[ "${confirmation}" == "true" ]]; then
     delete_storage_bucket "${PRIMUS_INPUT_STORAGE_BUCKET}"
   else
-    echo "Skipping the deletion of the bucket "${PRIMUS_INPUT_STORAGE_BUCKET}" ..."
+    echo "Skipping the deletion of the bucket ${PRIMUS_INPUT_STORAGE_BUCKET} ..."
   fi
 
-  confirmation=$(get_confirmation "Are you sure you want to delete the artifact registry "${PRIMUS_ARTIFACT_REPOSITORY}" ?")
+  confirmation=$(get_confirmation "Are you sure you want to delete the artifact registry ${PRIMUS_ARTIFACT_REPOSITORY} ?")
   if [[ "${confirmation}" == "true" ]]; then
     delete_artifact_repository "${PRIMUS_ARTIFACT_REPOSITORY}" "${PRIMUS_PROJECT_REPOSITORY_REGION}"
   else
-    echo "Skipping the deletion of the artifact registry "${PRIMUS_ARTIFACT_REPOSITORY}" ..."
+    echo "Skipping the deletion of the artifact registry ${PRIMUS_ARTIFACT_REPOSITORY} ..."
   fi
 
-  confirmation=$(get_confirmation "Are you sure you want to delete the workload identity pool "${PRIMUS_WORKLOAD_IDENTITY_POOL}" ?")
+  confirmation=$(get_confirmation "Are you sure you want to delete the workload identity pool ${PRIMUS_WORKLOAD_IDENTITY_POOL} ?")
   if [[ "${confirmation}" == "true" ]]; then
     delete_workload_identity_pool "${PRIMUS_WORKLOAD_IDENTITY_POOL}" "${PRIMUS_PROJECT_LOCATION}"
   else
-    echo "Skipping the deletion of the workload identity pool "${PRIMUS_WORKLOAD_IDENTITY_POOL}" ..."
+    echo "Skipping the deletion of the workload identity pool ${PRIMUS_WORKLOAD_IDENTITY_POOL} ..."
   fi
 
-  confirmation=$(get_confirmation "Are you sure you want to delete the serviceaccount "${PRIMUS_SERVICEACCOUNT}"@"${PRIMUS_PROJECT_ID}".iam.gserviceaccount.com ?")
+  confirmation=$(get_confirmation "Are you sure you want to delete the serviceaccount ${PRIMUS_SERVICEACCOUNT}@${PRIMUS_PROJECT_ID}.iam.gserviceaccount.com ?")
   if [[ "${confirmation}" == "true" ]]; then
     delete_service_account "${PRIMUS_SERVICEACCOUNT}"@"${PRIMUS_PROJECT_ID}".iam.gserviceaccount.com
   else
-    echo "Skipping the deletion of the serviceaccount "${PRIMUS_SERVICEACCOUNT}"@"${PRIMUS_PROJECT_ID}".iam.gserviceaccount.com ..."
+    echo "Skipping the deletion of the serviceaccount ${PRIMUS_SERVICEACCOUNT}@${PRIMUS_PROJECT_ID}.iam.gserviceaccount.com ..."
   fi
-}"
+}
 
 #######################################
 # Cleanup cloud resources of Secundus company.
@@ -72,39 +72,39 @@ delete_primus_company_resources() {
 #######################################
 delete_secundus_company_resources() {
   if [ ! -z "${SECUNDUS_PROJECT_ID}" ]; then
-    echo "SECUNDUS_PROJECT_ID is set to "${SECUNDUS_PROJECT_ID}""
+    echo "SECUNDUS_PROJECT_ID is set to ${SECUNDUS_PROJECT_ID}"
   else
     err "SECUNDUS_PROJECT_ID is not set, please set the SECUNDUS_PROJECT_ID with GCP project-id of Secundus company."
   fi
 
   set_gcp_project "${SECUNDUS_PROJECT_ID}"
 
-  confirmation=$(get_confirmation "Are you sure you want to delete the VM "${WORKLOAD_VM}" ?")
+  confirmation=$(get_confirmation "Are you sure you want to delete the VM ${WORKLOAD_VM} ?")
   if [[ "${confirmation}" == "true" ]]; then
     delete_vm "${WORKLOAD_VM}" "${SECUNDUS_PROJECT_ZONE}" "${SECUNDUS_PROJECT_ID}"
   else
-    echo "Skipping the deletion of the VM "${WORKLOAD_VM}" ..."
+    echo "Skipping the deletion of the VM ${WORKLOAD_VM} ..."
   fi
 
-  confirmation=$(get_confirmation "Are you sure you want to delete the bucket "${SECUNDUS_INPUT_STORAGE_BUCKET}" ?")
+  confirmation=$(get_confirmation "Are you sure you want to delete the bucket ${SECUNDUS_INPUT_STORAGE_BUCKET} ?")
   if [[ "${confirmation}" == "true" ]]; then
     delete_storage_bucket "${SECUNDUS_INPUT_STORAGE_BUCKET}"
   else
-    echo "Skipping the deletion of the bucket "${SECUNDUS_INPUT_STORAGE_BUCKET}" ..."
+    echo "Skipping the deletion of the bucket ${SECUNDUS_INPUT_STORAGE_BUCKET} ..."
   fi
 
-  confirmation=$(get_confirmation "Are you sure you want to delete the bucket "${SECUNDUS_RESULT_STORAGE_BUCKET}" ?")
+  confirmation=$(get_confirmation "Are you sure you want to delete the bucket ${SECUNDUS_RESULT_STORAGE_BUCKET} ?")
   if [[ "${confirmation}" == "true" ]]; then
     delete_storage_bucket "${SECUNDUS_RESULT_STORAGE_BUCKET}"
   else
-    echo "Skipping the deletion of the bucket "${SECUNDUS_RESULT_STORAGE_BUCKET}" ..."
+    echo "Skipping the deletion of the bucket ${SECUNDUS_RESULT_STORAGE_BUCKET} ..."
   fi
 
-  confirmation=$(get_confirmation "Are you sure you want to delete the serviceaccount "${WORKLOAD_SERVICEACCOUNT}"@"${SECUNDUS_PROJECT_ID}".iam.gserviceaccount.com ?")
+  confirmation=$(get_confirmation "Are you sure you want to delete the serviceaccount ${WORKLOAD_SERVICEACCOUNT}@${SECUNDUS_PROJECT_ID}.iam.gserviceaccount.com ?")
   if [[ "${confirmation}" == "true" ]]; then
-    delete_service_account "${WORKLOAD_SERVICEACCOUNT}"@"${SECUNDUS_PROJECT_ID}".iam.gserviceaccount.com
+    delete_service_account "${WORKLOAD_SERVICEACCOUNT}@${SECUNDUS_PROJECT_ID}.iam.gserviceaccount.com"
   else
-    echo "Skipping the deletion of the serviceaccount "${WORKLOAD_SERVICEACCOUNT}"@"${SECUNDUS_PROJECT_ID}"".iam.gserviceaccount.com ..."
+    echo "Skipping the deletion of the serviceaccount ${WORKLOAD_SERVICEACCOUNT}@${SECUNDUS_PROJECT_ID}.iam.gserviceaccount.com ..."
   fi
 }
 
