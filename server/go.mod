216ed12c24f76d47289fdbcb923cb9cf290f0d10
Confidential Space <no-reply@google.com>
2026-05-04 14:29:40 -0700

    No public description
    PiperOrigin-RevId: 910244820

--- server/go.mod
+++ server/go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/golang/glog v1.2.2
 	github.com/google/go-cmp v0.7.0
 	github.com/google/go-configfs-tsm v0.3.3-0.20240919001351-b4b5b84fdcbc
-	github.com/google/go-eventlog v0.0.3-0.20260305053119-5cd85087f9f9
+	github.com/google/go-eventlog v0.0.3-0.20260416001248-6807b85eecf0
 	github.com/google/go-tdx-guest v0.3.2-0.20250814004405-ffb0869e6f4d
 	github.com/google/go-tpm v0.9.6
 	github.com/google/go-tpm-tools v0.4.9-0.20260325175049-22911efba9e5
--- server/proto/buildprotos.sh
+++ server/proto/buildprotos.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-protoc -I. \
-  -I$(go list -mod=readonly -m -f "{{.Dir}}" github.com/google/go-tpm-tools) \
-  -I$(go list -mod=readonly -m -f "{{.Dir}}" github.com/google/go-tpm-tools)/proto \
-  -I`go list -mod=readonly -m -f "{{.Dir}}" github.com/google/go-sev-guest` \
-  -I`go list -mod=readonly -m -f "{{.Dir}}" github.com/google/go-tdx-guest` \
+# Collect include paths, only adding them if they are not empty.
+INCLUDES=("-I.")
+
+add_include() {
+  local module="$1"
+  local path
+  path=$(go list -mod=readonly -m -f "{{.Dir}}" "$module" 2>/dev/null)
+  if [ -n "$path" ]; then
+    INCLUDES+=("-I$path")
+    # Also add /proto subdirectory if it exists.
+    if [ -d "$path/proto" ]; then
+      INCLUDES+=("-I$path/proto")
+    fi
+  else
+    echo "Warning: Module $module not found, skipping." >&2
+  fi
+}
+
+add_include "github.com/google/go-tpm-tools"
+add_include "github.com/google/go-eventlog"
+add_include "github.com/google/go-sev-guest"
+add_include "github.com/google/go-tdx-guest"
+
+protoc "${INCLUDES[@]}" \
   -I/usr/local/include \
   --go_out=. \
   --go_opt=module=github.com/GoogleCloudPlatform/confidential-space/server/proto \
