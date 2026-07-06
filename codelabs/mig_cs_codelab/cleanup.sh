4723dd584d7d125cd65704cabb676d930b78e802
Confidential Space <no-reply@google.com>
2026-07-06 13:44:17 -0700

    No public description
    PiperOrigin-RevId: 943474602

--- codelabs/mig_cs_codelab/cleanup.sh
+++ codelabs/mig_cs_codelab/cleanup.sh
@@ -4,7 +4,6 @@
 
 source config_env.sh
 source common.sh
-
 #######################################
 # Cleanup cloud resources of UWEAR.
 # Globals:
@@ -19,22 +18,24 @@ source common.sh
 # Arguments:
 #   None
 #######################################
+
 delete_resources() {
   if [[ -n "${CURRENT_PROJECT_ID}" ]]; then
     echo "CURRENT_PROJECT_ID is set to ${CURRENT_PROJECT_ID}"
   else
     err "CURRENT_PROJECT_ID is not set, please set the CURRENT_PROJECT_ID with GCP project-id of current project."
+    exit 1
   fi
   set_gcp_project "${CURRENT_PROJECT_ID}"
-  delete_artifact_repository "${CURRENT_ARTIFACT_REPOSITORY}" "${CURRENT_PROJECT_REPOSITORY_REGION}"
+  delete_artifact_repository "${CURRENT_ARTIFACT_REPOSITORY:?Error: CURRENT_ARTIFACT_REPOSITORY is not set or empty. Please set it in config_env.sh and run 'source config_env.sh' first.}" "${CURRENT_PROJECT_REPOSITORY_REGION:?Error: CURRENT_PROJECT_REPOSITORY_REGION is not set or empty. Please set it in config_env.sh and run 'source config_env.sh' first.}"
 
 
-  gcloud compute instance-groups managed delete "${CURRENT_MIG_NAME}" --zone "${CURRENT_PROJECT_ZONE}"
-  gcloud compute health-checks delete "${HEALTH_CHECK_NAME}" --global
+  gcloud compute instance-groups managed delete "${CURRENT_MIG_NAME:?Error: CURRENT_MIG_NAME is not set or empty. Please set it in config_env.sh and run 'source config_env.sh' first.}" --zone "${CURRENT_PROJECT_ZONE:?Error: CURRENT_PROJECT_ZONE is not set or empty. Please set it in config_env.sh and run 'source config_env.sh' first.}"
+  gcloud compute health-checks delete "${HEALTH_CHECK_NAME:?Error: HEALTH_CHECK_NAME is not set or empty. Please set it in config_env.sh and run 'source config_env.sh' first.}" --global
   gcloud compute instance-groups managed stop-autoscaling "${CURRENT_MIG_NAME}" --zone "${CURRENT_PROJECT_ZONE}"
-  gcloud compute instance-templates delete "${TEMPLATE_NAME}" --zone "${CURRENT_PROJECT_ZONE}"
+  gcloud compute instance-templates delete "${TEMPLATE_NAME:?Error: TEMPLATE_NAME is not set or empty. Please set it in config_env.sh and run 'source config_env.sh' first.}" --zone "${CURRENT_PROJECT_ZONE}"
 
-  delete_service_account "${CURRENT_WORKLOAD_SERVICE_ACCOUNT}"@"${CURRENT_PROJECT_ID}".iam.gserviceaccount.com
+  delete_service_account "${CURRENT_WORKLOAD_SERVICE_ACCOUNT:?Error: CURRENT_WORKLOAD_SERVICE_ACCOUNT is not set or empty. Please set it in config_env.sh and run 'source config_env.sh' first.}"@"${CURRENT_PROJECT_ID}".iam.gserviceaccount.com
 }
 
 main() {
--- codelabs/mig_cs_codelab/create_service_accounts.sh
+++ codelabs/mig_cs_codelab/create_service_accounts.sh
@@ -5,17 +5,9 @@
 source config_env.sh
 source common.sh
 
-set_gcp_project "${CURRENT_PROJECT_ID}"
+set_gcp_project "${CURRENT_PROJECT_ID:?Error: CURRENT_PROJECT_ID is not set or empty. Please set it in config_env.sh and run 'source config_env.sh' first.}"
 
-echo "Creating workload service-account ${CURRENT_WORKLOAD_SERVICE_ACCOUNT} under project ${CURRENT_PROJECT_ID}..."
-create_service_account "${CURRENT_WORKLOAD_SERVICE_ACCOUNT}"
-grant_service_account_user_role_to_workload_operator "${CURRENT_WORKLOAD_SERVICE_ACCOUNT}" "${CURRENT_PROJECT_ID}"
-grant_workload_user_role_to_service_account "${CURRENT_WORKLOAD_SERVICE_ACCOUNT}" "${CURRENT_PROJECT_ID}"
-grant_log_writer_role_to_service_account "${CURRENT_WORKLOAD_SERVICE_ACCOUNT}" "${CURRENT_PROJECT_ID}"
-
-set_gcp_project "${CURRENT_PROJECT_ID}"
-
-echo "Creating workload service-account ${CURRENT_WORKLOAD_SERVICE_ACCOUNT} under project ${CURRENT_PROJECT_ID}..."
+echo "Creating workload service-account ${CURRENT_WORKLOAD_SERVICE_ACCOUNT:?Error: CURRENT_WORKLOAD_SERVICE_ACCOUNT is not set or empty. Please set it in config_env.sh and run 'source config_env.sh' first.} under project ${CURRENT_PROJECT_ID}..."
 create_service_account "${CURRENT_WORKLOAD_SERVICE_ACCOUNT}"
 
 grant_service_account_user_role_to_workload_operator "${CURRENT_WORKLOAD_SERVICE_ACCOUNT}" "${CURRENT_PROJECT_ID}"
